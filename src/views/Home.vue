1216c413a05e061e145cafed327c7c5e313a570e
Tavia Sterling <TaviaSterling@outlook.com>
2018-10-25 13:05:18 -0600

    parallax

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <img src="../assets/rake.png" class="icon">
+          <div class="navbar-brand">Raking and Shoveling by Cobus</div>
+          <img src="../assets/shovel.png" class="icon">
+        
+        
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto offset-sm-9">
+              
+              <li class="nav-item active">
+                <a class="nav-link active" href="#contact">Contact</a>
+              </li>
+              <li class="nav-item offset-sm-4">
+                <a class="nav-link active" href="#about">About</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div class="parallax"></div>
+        <div id="about" class="snow"><HelloWorld/></div>
+   
+   <div id="contact" class="grass"></div>
   </div>
 </template>
 
@@ -16,3 +36,42 @@ export default {
   }
 }
 </script>
+<style scoped>
+
+.icon {
+  height:40px;
+  width:40px;
+  margin-right:10px;
+ 
+}
+.navbar {
+  margin-top: 50px;
+}
+.parallax {
+    /* The image used */
+    background-image: url("../assets/leaves.jpeg");
+
+    /* Set a specific height */
+    min-height: 600px; 
+
+    /* Create the parallax scrolling effect */
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+.snow {
+    border-top:80px solid black;
+    border-bottom: 100px solid black;
+    background-image: url("../assets/snow.jpeg");
+    min-height: 800px;
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+.grass {
+  background-image: url("../assets/grass.jpeg");
+  min-height:600px;
+}
+</style>
